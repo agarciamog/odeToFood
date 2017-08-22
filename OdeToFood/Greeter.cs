29b12d1b15c7667657207c775cb92bc5966710d5
Alberto Garcia <agarciamog@gmail.com>
2017-08-21 19:55:16 -0600

    Added IGreeter and class that implements it, Greeter. Greeter accepts Configuration that comes from Startup. Startup Registers IGreeter as a service, also registered IConfiguration Configuration for IGreeter.

---
+++ OdeToFood/Greeter.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OdeToFood
+{
+    public interface IGreeter
+    {
+        string GetGreeter();
+    }
+    public class Greeter : IGreeter
+    {
+        private string greeting;
+
+        public Greeter(IConfiguration Configuration)
+        {
+            greeting = Configuration["greeting"];
+        }
+        public string GetGreeter()
+        {
+            return greeting;
+        }
+    }
+}
